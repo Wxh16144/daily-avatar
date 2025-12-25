bfc67be43c268176b5c65086a4cef5b0d8d21358
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-12-25 18:33:36 +0800

    prune

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,13 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const slogan = `魔法师正在进行最后的仪式，为您带来一项惊艳功能`;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <h3>{slogan}</h3>
+      <code hidden>src/app.tsx</code>
     </>
-  )
+  );
 }
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact()
+  ],
+  build: {
+    lib: {
+      entry: './src/index.js',
+      name: 'V2EX_daily_avatar',
+      fileName: 'main',
+      formats: ['iife']
+    },
+    rollupOptions: {
+      output: {
+        extend: true,
+        inlineDynamicImports: true,
+        manualChunks: undefined
+      }
+    },
+    minify: 'terser',
+    cssCodeSplit: false,
+    sourcemap: false
+  }
+});
